64f1f06bf48e3b2338cf06b02aebabc156eaa9c1
colinfrugte <colin.frugte@gmail.com>
2025-10-17 14:52:27 +0200

    frontend works

---
+++ src/app/App/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../Header/page";
+import TextInputToApi from "../TextInputToApi/page";
+
+export default function App() {
+  return (
+    <div className="flex justify-center items-center  font-stretch-condensed">
+      <Header />
+      <TextInputToApi />
+    </div>
+  );
+}
